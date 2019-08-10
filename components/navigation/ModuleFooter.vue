5b39f6b762604f78f33b750bfa4a3377f3c9099e
Josh <josh-king@users.noreply.github.com>
2019-08-10 18:58:42 +0100

    dev(intro): Working on the intro screen

---
+++ components/navigation/ModuleFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p class="palace-footer-text">
+        <strong>Mailing list &nbsp;&nbsp;&nbsp;&nbsp; Boring stuff</strong>
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.palace-footer-text {
+  text-transform: uppercase;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,37 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        palace-universal
-      </h1>
-      <h2 class="subtitle">
-        A remake of the Palace Skateboards website with universal rendering
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <section class="hero is-fullheight">
+    <div class="hero-body">
+      <div class="container">
+        <div class="columns is-centered">
+          <div
+            v-for="({ name, location }, id) in navigation"
+            :key="`palace-nav-${id}`"
+            class="column is-3"
+          >
+            <p class="has-text-centered">{{ name }}</p>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
+    <div class="hero-foot">
+      <module-footer />
+    </div>
+  </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import navigation from '@/assets/data/navigation'
+
+import ModuleFooter from '@/components/navigation/ModuleFooter'
 
 export default {
   components: {
-    Logo
-  }
+    ModuleFooter
+  },
+  data: () => ({
+    navigation
+  })
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style></style>
